062cde8045216927645c611f3dc1613676e05547
gyttaaa <bertaanggita@gmail.com>
2025-04-30 15:25:59 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Happy Birthday Ary, May laughter follow you wherever you go and dreams find their way to grow with you').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
